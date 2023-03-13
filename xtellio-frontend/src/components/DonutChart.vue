1f1796044f15c58d150cb796f1df1679c4fdc8e1
Bubble <emiltols2001@outlook.dk>
2023-03-13 11:16:41 +0100

    Fixed Christians lort

--- xtellio-frontend/src/components/DonutChart.vue
+++ xtellio-frontend/src/components/DonutChart.vue
@@ -4,100 +4,93 @@ export default {
   name: 'DonutChart',
   data() {
     return {
-        series: [44, 55, 13, 33],
-          chartOptions: {
+      series: [44, 55, 13, 33],
+      chartOptions: {
+        chart: {
+          width: 380,
+          type: 'donut',
+        },
+        dataLabels: {
+          enabled: false
+        },
+        labels: ['Team A', 'Team B', 'Team C', 'Team D'],
+        responsive: [{
+          breakpoint: 480,
+          options: {
             chart: {
-              width: 380,
-              type: 'donut',
+              width: 200
             },
-            dataLabels: {
-              enabled: false
-            },
-            labels: ['Team A', 'Team B', 'Team C', 'Team D'],
-            responsive: [{
-              breakpoint: 480,
-              options: {
-                chart: {
-                  width: 200
-                },
-                legend: {
-                  show: false
-                }
-              }
-            }],
             legend: {
-              position: 'right',
-              offsetY: 0,
-              height: 230,
+              show: false
             }
-          },
-        methods: {
-          appendData: function () {
-            var arr = this.series.slice()
-            arr.push(Math.floor(Math.random() * (100 - 1 + 1)) + 1)
-            this.series = arr
-          },
-        
-          removeData: function () {
-            if(this.series.length === 1) return
-            var arr = this.series.slice()
-            arr.pop()
-            this.series = arr
-          },
-        
-          randomize: function () {
-            this.series = this.series.map(function() {
-              return Math.floor(Math.random() * (100 - 1 + 1)) + 1
-            })
-          },
-        
-          reset: function () {
-            this.series = [44, 55, 13, 33]
           }
+        }],
+        legend: {
+          position: 'right',
+          offsetY: 0,
+          height: 230,
+        }
+      },
+    }
+  },
+  methods: {
+    whatever() {
+      console.log("Hello")
+    },
+    appendData() {
+      console.log("Pressed")
+      var arr = this.series.slice()
+      arr.push(Math.floor(Math.random() * (100 - 1 + 1)) + 1)
+      this.series = arr
+    },
+
+    removeData() {
+      if (this.series.length === 1) return
+      var arr = this.series.slice()
+      arr.pop()
+      this.series = arr
+    },
+
+    randomize() {
+      this.series = this.series.map(function () {
+        return Math.floor(Math.random() * (100 - 1 + 1)) + 1
+      })
+    },
+
+    reset() {
+      this.series = [44, 55, 13, 33]
     }
   }
-}}
+}
 
 </script>
 
 <template>
   <div>
-        <div class="chart-wrap">
-          <div id="chart">
+    <div class="chart-wrap">
+      <div id="chart">
         <apexchart type="donut" width="380" :options="chartOptions" :series="series"></apexchart>
       </div>
-        </div>
-      
-        <div class="actions">
-          <button
-              
-              
-              @click="appendData">
-            + ADD
-          </button>
-          
-          <button
-              
-              
-              @click="removeData">
-            - REMOVE
-          </button>
-          
-          <button
-              
-              
-              @click="randomize">
-            RANDOMIZE
-          </button>
-          
-          <button
-              
-              
-              @click="reset">
-            RESET
-          </button>
-        </div>
-      </div>
+    </div>
+
+    <div class="actions" style="display: flex; gap: 25px;">
+      <button @click="appendData">
+        + ADD
+      </button>
+
+      <button @click="removeData">
+        - REMOVE
+      </button>
+
+      <button @click="randomize">
+        RANDOMIZE
+      </button>
+
+      <button @click="reset">
+        RESET
+      </button>
+    </div>
+  </div>
 </template>
 
 <style></style>
