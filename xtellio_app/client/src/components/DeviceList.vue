2b5ce3d65c8d917425d0b60d0a6f59bf3d0fc7ab
fundish <funderchristian@gmail.com>
2023-03-29 09:19:27 +0200

    Update DeviceList.vue

--- xtellio_app/client/src/components/DeviceList.vue
+++ xtellio_app/client/src/components/DeviceList.vue
@@ -1,23 +1,19 @@
 <script>
-
+import DeviceService from '../DeviceService';
 export default {
   name: 'DeviceList',
   data() {
     return {
-      options: {
-        chart: {
-          id: 'vuechart-example'
-        },
-        xaxis: {
-          categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998]
-        }
-      },
-      series: [{
-        name: 'series-1',
-        data: [30, 40, 45, 50, 49, 60, 70, 91, 100]
-      }]
+      devices: []
     }
-  }
+  },
+  async created() {
+        try {
+            this.temp = await DeviceService.getAllDevices();
+        } catch (err) {
+            this.error = err.message
+        }
+    }
 }
 
 </script>
