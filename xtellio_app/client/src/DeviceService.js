b91b8d632df64dad421f9f72982d7d68ca64f026
Bubble <emiltols2001@outlook.dk>
2023-04-12 10:04:34 +0200

    Update DeviceService.js

--- xtellio_app/client/src/DeviceService.js
+++ xtellio_app/client/src/DeviceService.js
@@ -31,23 +31,6 @@ class DeviceService {
       }
     })
   }
-
-  static Test() {
-    const url = 'https://admin.xtellio.com/web/devices';
-    const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMDQwNzQ5OEB1Y24uZGsiLCJ0aWQiOiJMYWdlciBYdGVsOmFhYmRiMzI4NmQ4NDQ3ZWFhZWFlOTQ0YTdlMTNkZGZmIiwiYXVkIjoic2VydmVyOnRva2VuIn0.n1wKPHnk6Aue8ZJEHnlOTHCHBQ2BwpL0OeQqDjtwDxc';
-
-    axios.get(url, {
-      headers: {
-        'Authorization': `Bearer ${token}`
-      }
-    })
-    .then(response => {
-      console.log(response)
-    })
-    .catch(error => {
-      // handle error
-    });
-  }
 }
 
 export default DeviceService;
