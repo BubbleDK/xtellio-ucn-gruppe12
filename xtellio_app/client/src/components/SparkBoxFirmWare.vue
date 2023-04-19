ff5d8141e7fb8fe654cac2a915906628f2eac881
fundish <funderchristian@gmail.com>
2023-04-19 09:10:02 +0200

    Update SparkBoxFirmWare.vue

--- xtellio_app/client/src/components/SparkBoxFirmWare.vue
+++ xtellio_app/client/src/components/SparkBoxFirmWare.vue
@@ -39,6 +39,9 @@ export default {
               distributed: true,
           }
         },
+        dataLabels: {
+          enabled: true,
+        },
         stroke: {
           curve: 'straight',
         },
