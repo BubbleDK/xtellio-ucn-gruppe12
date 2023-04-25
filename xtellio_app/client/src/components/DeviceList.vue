9fca19ab470a95a5c5b1dfd73dbd427f4e2c1be8
JakobUCN <jkjeldsteen@gmail.com>
2023-04-25 12:11:29 +0200

    Small styling/design updates

--- xtellio_app/client/src/components/DeviceList.vue
+++ xtellio_app/client/src/components/DeviceList.vue
@@ -32,6 +32,7 @@ export default {
       lastLogOldList: [],
       macAddressInput: '',
       firmwareInput: '',
+      selected: 'Sort',
       filters: [
         {
           id: 'org',
@@ -81,7 +82,7 @@ export default {
         },
       ],
       sortOptions: [
-        { name: 'Type A-Z', current: true },
+        { name: 'Type A-Z', current: false },
         { name: 'Type Z-A', current: false },
         { name: 'Customer A-Z', current: false },
         { name: 'Customer Z-A', current: false },
@@ -230,6 +231,7 @@ export default {
         else{
           element.current = true;
           this.sortedList(element)
+          this.selected = element.name
         }
       }
     },
@@ -285,8 +287,9 @@ const mobileFiltersOpen = ref(false)
             <Menu as="div" class="relative inline-block text-left">
               <div>
                 <MenuButton
-                  class="group inline-flex justify-center text-sm font-medium text-gray-700 hover:text-white">
-                  Sort
+                  class="group inline-flex justify-center text-sm font-medium text-gray-300 hover:text-white"
+                  >
+                  <p>{{ selected }}</p>
                   <ChevronDownIcon class="-mr-1 ml-1 h-5 w-5 flex-shrink-0 text-gray-400 group-hover:text-white"
                     aria-hidden="true" />
                 </MenuButton>
@@ -308,11 +311,6 @@ const mobileFiltersOpen = ref(false)
                 </MenuItems>
               </transition>
             </Menu>
-
-            <button type="button" class="-m-2 ml-5 p-2 text-gray-400 hover:text-white sm:ml-7">
-              <span class="sr-only">View grid</span>
-              <Squares2X2Icon class="h-5 w-5" aria-hidden="true" />
-            </button>
           </div>
         </div>
 
@@ -364,23 +362,8 @@ const mobileFiltersOpen = ref(false)
                   <thead class="dark:bg-gray-800">
                     <tr>
                       <th scope="col"
-                        class="py-3.5 px-4 text-sm font-normal text-left rtl:text-right text-white dark:text-gray-400">
-                        <div class="flex items-center gap-x-3">
-                          <button class="flex items-center gap-x-2">
-                            <span>Type</span>
-                            <svg class="h-3" viewBox="0 0 10 11" fill="none" xmlns="http://www.w3.org/2000/svg">
-                              <path
-                                d="M2.13347 0.0999756H2.98516L5.01902 4.79058H3.86226L3.45549 3.79907H1.63772L1.24366 4.79058H0.0996094L2.13347 0.0999756ZM2.54025 1.46012L1.96822 2.92196H3.11227L2.54025 1.46012Z"
-                                fill="currentColor" stroke="currentColor" stroke-width="0.1" />
-                              <path
-                                d="M0.722656 9.60832L3.09974 6.78633H0.811638V5.87109H4.35819V6.78633L2.01925 9.60832H4.43446V10.5617H0.722656V9.60832Z"
-                                fill="currentColor" stroke="currentColor" stroke-width="0.1" />
-                              <path
-                                d="M8.45558 7.25664V7.40664H8.60558H9.66065C9.72481 7.40664 9.74667 7.42274 9.75141 7.42691C9.75148 7.42808 9.75146 7.42993 9.75116 7.43262C9.75001 7.44265 9.74458 7.46304 9.72525 7.49314C9.72522 7.4932 9.72518 7.49326 9.72514 7.49332L7.86959 10.3529L7.86924 10.3534C7.83227 10.4109 7.79863 10.418 7.78568 10.418C7.77272 10.418 7.73908 10.4109 7.70211 10.3534L7.70177 10.3529L5.84621 7.49332C5.84617 7.49325 5.84612 7.49318 5.84608 7.49311C5.82677 7.46302 5.82135 7.44264 5.8202 7.43262C5.81989 7.42993 5.81987 7.42808 5.81994 7.42691C5.82469 7.42274 5.84655 7.40664 5.91071 7.40664H6.96578H7.11578V7.25664V0.633865C7.11578 0.42434 7.29014 0.249976 7.49967 0.249976H8.07169C8.28121 0.249976 8.45558 0.42434 8.45558 0.633865V7.25664Z"
-                                fill="currentColor" stroke="currentColor" stroke-width="0.3" />
-                            </svg>
-                          </button>
-                        </div>
+                        class="px-6 py-3.5 text-sm font-normal text-left rtl:text-right text-white dark:text-gray-400">
+                        Type
                       </th>
                       <th scope="col"
                         class="px-6 py-3.5 text-sm font-normal text-left rtl:text-right text-white dark:text-gray-400">
@@ -410,7 +393,9 @@ const mobileFiltersOpen = ref(false)
                         class="px-6 py-3.5 text-sm font-normal text-left rtl:text-right text-white dark:text-gray-400">
                         Firmware
                       </th>
-                      <th scope="col" class="relative py-3.5 px-4">
+                      <th scope="col" 
+                        class="px-6 py-3.5 text-sm font-normal text-left rtl:text-right text-white dark:text-gray-400">
+                        Details
                         <span class="sr-only">Actions</span>
                       </th>
                     </tr>
@@ -468,9 +453,9 @@ const mobileFiltersOpen = ref(false)
                         {{ device?.status?.sw }}
                       </td>
                       <td class="px-6 py-4 text-sm font-medium text-right whitespace-nowrap">
-                        <a class="text-green-500 hover:text-green-700" @click="goTodetail(device.mac)">
+                        <button class="text-green-500 hover:text-green-700" @click="goTodetail(device.mac)">
                           View
-                        </a>
+                        </button>
                       </td>
                     </tr>
                   </tbody>
--- xtellio_app/client/src/components/NavBar.vue
+++ xtellio_app/client/src/components/NavBar.vue
@@ -13,9 +13,6 @@
         </div>
         <div class="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
           <div class="flex flex-shrink-0 items-center">
-            <img class="block h-8 w-auto lg:hidden"
-              src="https://assets.website-files.com/62220ed2c8d90b8198d472f3/62989ce3fb258e42723edd10_Xtellio.png"
-              alt="Your Company" />
             <img class="hidden h-8 w-auto lg:block"
               src="https://assets.website-files.com/62220ed2c8d90b8198d472f3/62989ce3fb258e42723edd10_Xtellio.png"
               alt="Your Company" />
@@ -27,44 +24,6 @@
                 :aria-current="item.current ? 'page' : undefined">{{ item.name }}</router-link>
             </div>
           </div>
-        </div>
-        <div class="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-          <button type="button"
-            class="rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
-            <span class="sr-only">View notifications</span>
-            <BellIcon class="h-6 w-6" aria-hidden="true" />
-          </button>
-
-          <!-- Profile dropdown -->
-          <Menu as="div" class="relative ml-3">
-            <div>
-              <MenuButton
-                class="flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
-                <span class="sr-only">Open user menu</span>
-                <img class="h-8 w-8 rounded-full"
-                  src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b5/Windows_10_Default_Profile_Picture.svg/768px-Windows_10_Default_Profile_Picture.svg.png"
-                  alt="" />
-              </MenuButton>
-            </div>
-            <transition enter-active-class="transition ease-out duration-100"
-              enter-from-class="transform opacity-0 scale-95" enter-to-class="transform opacity-100 scale-100"
-              leave-active-class="transition ease-in duration-75" leave-from-class="transform opacity-100 scale-100"
-              leave-to-class="transform opacity-0 scale-95">
-              <MenuItems
-                class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                <MenuItem v-slot="{ active }">
-                <a href="#" :class="[active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700']">Your
-                  Profile</a>
-                </MenuItem>
-                <MenuItem v-slot="{ active }">
-                <a href="#" :class="[active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700']">Settings</a>
-                </MenuItem>
-                <MenuItem v-slot="{ active }">
-                <a href="#" :class="[active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700']">Sign out</a>
-                </MenuItem>
-              </MenuItems>
-            </transition>
-          </Menu>
         </div>
       </div>
     </div>
@@ -84,7 +43,6 @@ import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/vue/24/outline';
 
 const navigation = [
   { name: 'Dashboard', href: '/', current: true },
-  // { name: 'Example', href: '/example', current: false },
   { name: 'Devices', href: '/devicelistview', current: false },
   { name: 'Customers', href: '/orgview', current: false },
 ]
